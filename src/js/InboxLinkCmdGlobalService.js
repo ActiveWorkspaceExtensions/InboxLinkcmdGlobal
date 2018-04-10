91cc6ce99ce79e209e5cad05ac4cfe40ef7236e6
Luke Seiler <luke.seiler@siemens.com>
2018-04-10 14:35:20 -0600

    added another global command for the Home folder

---
+++ src/js/InboxLinkCmdGlobalService.js
@@ -0,0 +1,20 @@
+// <!-- Copyright (c) 2017 Siemens Product Lifecycle Management Software Inc. Licensed under the "MPL 2.0 License" -->
+
+define(["app", "js/logService", "js/browserUtils", "js/locationNavigation.service", "js/NotyModule"], //
+    function (app, logger, browserUtils) {
+        'use strict';
+        var exports = {},
+            _$state = null;
+
+        exports.go = function (location, uUID) {
+                _$state = browserUtils.getBaseURL() + location + uUID;
+                window.open(_$state, "_self");
+            },
+
+            exports.goHomeYourDrunk = function (location, uUID) {
+                _$state = browserUtils.getBaseURL() + location + uUID + "&uid=" + uUID;
+                window.open(_$state, "_self");
+            }
+        return exports;
+
+    });
